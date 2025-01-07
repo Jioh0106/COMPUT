7f56a9215e03e5f3af467e3ec1732dbeccf3f444
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2025-01-07 13:02:57 +0900

    Update : 공통코드관리 페이지의 공통코드, 상세공통코드 조회 처리

--- src/main/java/com/deepen/service/CommonService.java
+++ src/main/java/com/deepen/service/CommonService.java
@@ -2,18 +2,10 @@ package com.deepen.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-import com.deepen.domain.BoardDTO;
-import com.deepen.domain.CommonDetailDTO;
-import com.deepen.entity.Board;
-import com.deepen.entity.CommonDetail;
 import com.deepen.mapper.CommonMapper;
-import com.deepen.repository.BoardRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -22,14 +14,15 @@ import lombok.extern.java.Log;
 @RequiredArgsConstructor
 @Log
 public class CommonService {
-	
+
 	private final CommonMapper mapper;
-	
+
 	public List<Map<String, Object>> commonList() {
 		return mapper.commonList();
 	}
-	
-	
+
+	public List<Map<String, Object>> commonDtlList(String commonCd) {
+		return mapper.commonDtlList(commonCd);
+	}
 
 }
-
