6dc16c97ba0df424c6f9c0da13a34b8a635b5b10
jioh <bonnyjo106@naver.com>
2025-01-08 09:42:17 +0900

    Delete : 테스트용 member, board 관련 파일 전부 삭제

--- src/main/java/com/deepen/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deepen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.deepen.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-
-}
