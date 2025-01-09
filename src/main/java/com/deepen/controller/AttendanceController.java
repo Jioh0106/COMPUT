d218cfb8e8230bf21bb37281e833509150746edb
jioh <bonnyjo106@naver.com>
2025-01-09 14:24:44 +0900

    update : loab-mng/crud

--- src/main/java/com/deepen/controller/AttendanceController.java
+++ src/main/java/com/deepen/controller/AttendanceController.java
@@ -1,21 +1,7 @@
 package com.deepen.controller;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.deepen.domain.CommonDetailDTO;
-import com.deepen.service.AttendanceService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -25,7 +11,6 @@ import lombok.extern.java.Log;
 @Log
 public class AttendanceController {
 	
-	private final AttendanceService attendanceService;
 	
 	// 출퇴근 현황
 	@GetMapping("/cmt-stts")
@@ -41,61 +26,7 @@ public class AttendanceController {
 		return "attendance/vctn_mng";
 	}
 	
-	// 휴직 관리
-	@GetMapping("/loab-mng")
-	public String absence(Model model) throws JsonProcessingException {
-		//http://localhost:8082/loab-mng
-		
-		List<Map<String, Object>> absenceList = attendanceService.getAbsenceList();
-		
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	    absenceList.forEach(item -> {
-	        Timestamp timestamp = (Timestamp) item.get("ABSENCE_START");
-	        if (timestamp != null) {
-	            LocalDateTime dateTime = timestamp.toLocalDateTime();
-	            String formattedDate = dateTime.format(formatter);
-	            item.put("ABSENCE_START", formattedDate);
-	        }
-	        Timestamp timestamp2 = (Timestamp) item.get("ABSENCE_END");
-	        if (timestamp2 != null) {
-	        	LocalDateTime dateTime = timestamp2.toLocalDateTime();
-	        	String formattedDate = dateTime.format(formatter);
-	        	item.put("ABSENCE_END", formattedDate);
-	        }
-	        Timestamp timestamp3 = (Timestamp) item.get("REQUEST_DATE");
-	        if (timestamp3 != null) {
-	        	LocalDateTime dateTime = timestamp3.toLocalDateTime();
-	        	String formattedDate = dateTime.format(formatter);
-	        	item.put("REQUEST_DATE", formattedDate);
-	        }
-	    });
-	    
-		log.info("absenceList : " + absenceList.toString());
-		
-		model.addAttribute("absenceList", absenceList);
-		
-		return "attendance/loab_mng";
-	}
 	
-	
-	@PostMapping("/loab-insert")
-	public String laobInsert(String emp_id, String absence_start, String absence_end, String absence_type, String absence_remark ) {
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("emp_id", map);
-		map.put("absence_start", absence_start);
-		map.put("absence_end", absence_end);
-		map.put("absence_type", absence_type);
-		map.put("absence_remark", absence_remark);
-		map.put("request_type", "휴직");
-		
-		attendanceService.insertLoab(map);
-		
-		
-		
-		
-		return "result";
-	}
 	
 	// 근무 관리
 	@GetMapping("/work-mng")
