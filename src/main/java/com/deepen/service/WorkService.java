3d5772844becd27c91e20f5d0fc1c41d3e11a379
jioh <bonnyjo106@naver.com>
2025-01-10 14:43:01 +0900

    add : WorkController, WorkService

---
+++ src/main/java/com/deepen/service/WorkService.java
@@ -0,0 +1,13 @@
+package com.deepen.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@RequiredArgsConstructor
+@Service
+@Log
+public class WorkService {
+
+}
