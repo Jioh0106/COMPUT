4822b1b37068ef935e5703b98ce6dbb6d64d8a5e
namepsh <psh94339191@gmail.com>
2025-01-20 21:52:51 +0900

    Update : 출/퇴근 버튼 구현(휴가랑 연계는 아직 x)

--- src/main/java/com/deepen/service/CommuteService.java
+++ src/main/java/com/deepen/service/CommuteService.java
@@ -43,8 +43,10 @@ public class CommuteService {
 		String today = String.valueOf(now);
 		
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		System.out.println(timestamp);
-
+		
+		// 현재 시간을 HH:MM 형태로 포맷
+		String timeFormat = timestamp.getHours() + ":" +  timestamp.getMinutes();
+		
 		try {
             
             for(Map<String, Object> work : workList) {
@@ -53,27 +55,33 @@ public class CommuteService {
             	String workDate = String.valueOf(work.get("workDate"));
             	String workStart = String.valueOf(work.get("workStart"));
             	String workEnd = String.valueOf(work.get("workEnd"));
+            	String cmtCd = String.valueOf(work.get("cmtCd"));
             	String cmtDate = String.valueOf(work.get("cmtDate"));
             	String cmtStart = String.valueOf(work.get("cmtStart"));
             	String cmtEnd = String.valueOf(work.get("cmtEnd"));
             	String empNo = String.valueOf(work.get("empNo"));
             	String empDept = String.valueOf(work.get("empDept"));
             	
+            	
             	if(workDate.equals(today)) { // 근무일자가 오늘 날짜와 같으며
-            		if(cmtDate.equals("null")) { // 출근일이 없고
-            			if(cmtStart.equals("null")) { // 찍힌 출근 시간이 없고
+            		if(nullCheck(cmtDate)) { // 출근일이 없고
+            			if(nullCheck(cmtStart)) { // 찍힌 출근 시간이 없고
             				if(timestamp.equals(parseTime(workStart)) 
             						|| timestamp.before(parseTime(workStart))) { // 현재 일시가 출근 시간이랑 같거나 이전일 경우
 	            				cmtData.put("result", "start"); 
 	            				cmtData.put("empDept", empDept);
-	            				cmtData.put("cmtStart", parseTime(workStart));
+	            				cmtData.put("cmtCd", cmtCd);
+	            				cmtData.put("cmtStart", workStart);
 	            				cmtData.put("sttsCd", "CMTN001");
 	            				cmtData.put("empNo", empNo);
 	            				cmtData.put("empId", empId);
+	            				
             				} else { // 이후일 경우
             					cmtData.put("result", "start"); 
 	            				cmtData.put("empDept", empDept);
-	            				cmtData.put("cmtStart", timestamp);
+	            				cmtData.put("cmtCd", cmtCd);
+	            				cmtData.put("cmtStart", timeFormat);
+	            				cmtData.put("sttsCd", "CMTN002");
 	            				cmtData.put("empNo", empNo);
 	            				cmtData.put("empId", empId);
             				}
@@ -83,14 +91,16 @@ public class CommuteService {
             			}
             			
             		} else { // 출근일이 있고
-            			if(cmtEnd.equals("null")) { // 찍힌 퇴근 시간이 없고
+            			if(nullCheck(cmtEnd)) { // 찍힌 퇴근 시간이 없고
             				if(timestamp.equals(parseTime(workEnd)) 
             						|| timestamp.after(parseTime(workEnd))) { // 현재 일시가 퇴근 시간이랑 같거나 이후일 경우
 	            				cmtData.put("result", "end"); 
-	            				cmtData.put("cmtEnd", parseTime(workEnd));
+	            				cmtData.put("cmtCd", cmtCd);
+	            				cmtData.put("cmtEnd", workEnd);
             				} else {
             					cmtData.put("result", "end"); 
-	            				cmtData.put("cmtEnd", timestamp);
+            					cmtData.put("cmtCd", cmtCd);
+	            				cmtData.put("cmtEnd", timeFormat);
             				}
             				
             			} else {
@@ -129,11 +139,31 @@ public class CommuteService {
 		return timestamp;
 	}
 
-	public int insertCmt(Map<String, Object> searchMap) {
-		searchMap.put("sttsCd", "CMTN001");
-		System.out.println(searchMap.toString());
-		//int result = mapper.insertCmt(searchMap);
-		return 1;
+	public int insertCmt(Map<String, Object> searchMap) throws ParseException {
+		
+		int result = 0;
+		
+		String cmtTime = String.valueOf(parseTime(String.valueOf(searchMap.get("cmtTime"))));
+		
+		searchMap.put("cmtTime", cmtTime);
+
+		if(nullCheck(String.valueOf(searchMap.get("cmtCd")))) {
+			result = mapper.insertCmt(searchMap);
+		} else {
+			result = mapper.updateCmt(searchMap);
+		}
+		
+		return result;
+	}
+	
+	private boolean nullCheck(String str) {
+		
+		if(str.equals(null) || str.isEmpty() || str.equals("null") 
+				|| str == null || str == "null") {
+			return true;
+		}
+		
+		return false;
 	}
 
 }
