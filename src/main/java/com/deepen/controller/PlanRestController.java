48f463bdc0fa6c237f94f704c04395cdbd771fc2
jioh <bonnyjo106@naver.com>
2025-02-03 09:45:16 +0900

    ADD : 생산계획  PrdctPlan 파일 생성

---
+++ src/main/java/com/deepen/controller/PlanRestController.java
@@ -0,0 +1,28 @@
+package com.deepen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deepen.service.PrdctPlanService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/plan")
+@Log
+public class PlanRestController {
+	
+	/** 생산계획 서비스 */
+	private final PrdctPlanService service;
+	
+	
+	/** 생산계획 그리드 정보 저장 **/
+	
+	
+	/** 생산계획 그리드 정보 삭제 **/
+	
+	
+	
+} // PlanRestController
