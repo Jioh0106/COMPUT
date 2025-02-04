b3453874954ff307957f8050163f2527817e3e42
jioh <bonnyjo106@naver.com>
2025-02-04 09:57:24 +0900

    update : 자재 컬럼 변경

--- src/main/java/com/deepen/domain/MaterialDTO.java
+++ src/main/java/com/deepen/domain/MaterialDTO.java
@@ -1,12 +1,28 @@
 package com.deepen.domain;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class MaterialDTO {
+	
 	private Integer mtr_no;
 	private String mtr_name;
-	private String ress_unit;
-	private String use_unit;
+	private String mtr_type;
+	private String composition;
+	private String hardness;
+	private Integer density;
+	private Integer melting_point;
+	private Integer tensile_strength;
+	private String mtr_use;
+	private String mtr_unit;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate mtr_reg_data;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate mtr_mod_data;
 	private String mtr_status;
+	
 }
