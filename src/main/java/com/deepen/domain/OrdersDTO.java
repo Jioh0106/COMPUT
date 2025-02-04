bf0a1f9f08a85b468af5d3eb7defcd576845acb1
jioh <bonnyjo106@naver.com>
2025-02-04 16:24:05 +0900

    Add : Orders Entity, DTO 파일 추가

---
+++ src/main/java/com/deepen/domain/OrdersDTO.java
@@ -0,0 +1,23 @@
+package com.deepen.domain;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class OrdersDTO {
+
+	private String order_id;
+	private String order_emp;
+	private String order_type;
+	private Integer client_no;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	private Timestamp order_date;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+	private Timestamp rder_update;
+	
+}
